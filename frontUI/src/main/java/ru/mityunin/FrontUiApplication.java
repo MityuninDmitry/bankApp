5319d434618222166bf957f1163eb25936eea6f1
dmitrymityunin <mity-ml@mail.ru>
2025-07-07 23:20:34 +0500

    добавил модуль frontUI

---
+++ frontUI/src/main/java/ru/mityunin/FrontUiApplication.java
@@ -0,0 +1,11 @@
+package ru.mityunin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontUiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FrontUiApplication.class, args);
+    }
+}
