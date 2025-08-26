3fdc2ac47484a708ba8ec259a0af4146862116e2
dmitrymityunin <mity-ml@mail.ru>
2025-08-27 00:36:11 +0500

    Интеграция сервисов через консул

--- Accounts/src/main/java/ru/mityunin/AppConfig.java
+++ Accounts/src/main/java/ru/mityunin/AppConfig.java
@@ -1,5 +1,6 @@
 package ru.mityunin;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,6 +15,7 @@ public class AppConfig {
     }
 
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
